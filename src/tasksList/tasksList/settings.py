798a15eb61089c12a23e7a23c892f0e0fdc9e368
Javier Vera <javier.vera@clarite.pl>
2024-04-04 16:39:28 +0200

    Contains erros - need to be fixed

--- src/tasksList/tasksList/settings.py
+++ src/tasksList/tasksList/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7f7z67(#egv9kpl=i&%gqe5!2jj9k)=6#@7c0526n&7x^9o%bj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
